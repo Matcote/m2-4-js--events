91c7d190e68205d04b941db222b6aec4fd53b970
Matcote <matthewcotesipod@gmail.com>
2020-06-18 15:25:42 -0400

    exercise 1 done

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,19 @@
 // Exercise 1.1
 // ------------
-console.log('exercise 1.1');
+console.log("exercise 1.1");
+const body = document.querySelector("body");
+const result = document.querySelector(".result");
+let yesNo = "false";
+
+body.addEventListener("click", function () {
+  yesNo = "true";
+});
+setTimeout(function () {
+  if (yesNo === "false") {
+    result.innerText = "You Lose!";
+    console.log("notclicked");
+  } else if (yesNo === "true") {
+    result.innerText = "You Win!";
+    console.log("clicked");
+  }
+}, 1000);
--- workshop/exercise-1.2/app.js
+++ workshop/exercise-1.2/app.js
@@ -1,3 +1,24 @@
 // Exercise 1.2
 // ------------
-console.log('exercise 1.2');
+console.log("exercise 1.2");
+
+const body = document.querySelector("body");
+let result = document.querySelector(".result");
+let time = document.querySelector("#time");
+let random = Math.round(Math.random() * 5);
+let yesNo = "false";
+let timeLeft = "true";
+time.innerHTML = random;
+
+let myFunction = function () {
+  yesNo = "true";
+  result.innerText = "You Win!";
+};
+body.addEventListener("click", myFunction);
+
+let timeOut = setTimeout(function () {
+  if (yesNo === "false") {
+    result.innerText = "You Lose!";
+    body.removeEventListener("click", myFunction);
+  }
+}, random * 1000);
